fe9c2b37e0361395e0e524b1bc7e0eeeb7a54d59
acsearles <acsearles@gmail.com>
2016-05-16 11:36:30 -0500

    MONA-137 testing the checkbox and the checkboxlist

--- script.js
+++ script.js
@@ -9,11 +9,24 @@
   function TestController() {
     var vm = this;
     vm.name = 'World!';
+    
+    //Check Box Testing
     vm.checkbox = true;
     vm.checkboxField = {
-      name: 'asdsad',
-      label: 'asdasd'
+      name: 'Select this box',
+      label: 'Select this box'
     };
+    
+    //Check Box List Testing
+    vm.checkboxListFields = {
+      listItems: {
+        Orange: 'orange',
+        Green: 'green',
+        Blue: 'blue',
+        Pink: 'pink'
+      }
+    };
+    
   }
 
 })();
